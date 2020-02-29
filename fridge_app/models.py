9c9d412fc9de504086efaf1384d2b9348565de45
Jennifer Long <j.alicia.long@gmail.com>
2020-02-29 17:32:06 -0500

    fixed foodview

--- fridge_app/models.py
+++ fridge_app/models.py
@@ -1,27 +1,10 @@
 from django.db import models
 
-# Create your models here.
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    def __str__(self):
-        return self.question_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.question_text
-
-
-class FoodModel(models.Model):
+class Food(models.Model):
     user = models.CharField(max_length=15) # maybe autofill user to user.user_name but idk how?
-    food_in = models.CharField(max_length=20)
-    expiration_date = models.DateTimeField('expiration date')
+    food_name = models.CharField(max_length=100)
+    exp_date = models.DateTimeField('expiration date')
     def __str__(self):
         return self.food_in
--- fridge_app/views.py
+++ fridge_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-# from fridge_app.forms import FoodForm
-# from django.views.generic.edit import FormView
+from django.utils.decorators import method_decorator
+from django.views import generic
 
-# Create your views here.
+from .models import Food
+# from fridge_app.forms import FoodForm
 
 @login_required
 def index(request):
@@ -13,11 +14,19 @@ def index(request):
 def about(request):
     return render(request, 'about.html')
 
-# @login_required
-# def FoodView(formView):
-#     template name = 'food_form.html'
-#     form_class = FoodForm
-#     success_url = '/index/'
+# Can't use @login_required decorator on a class
+@method_decorator(login_required, name='dispatch')
+class LogFoodView(generic.CreateView):
+    model = Food
+    template_name = 'food_form.html'
+    success_url = 'index/'
+    fields = ['user', 'food_name', 'exp_date']
 
-#     def form_valid(self, form):
-#         return super().form_valid(form)
+# May want to use a form class instead for more customization
+# @login_required
+# def FoodView(request):
+#     template_name = 'food_form.html'
+#     form_class = FoodForm
+#     success_url = 'index/'
+    # def form_valid(self, form):
+    #     return super().form_valid(form)
