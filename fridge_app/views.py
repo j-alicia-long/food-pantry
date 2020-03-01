85cc5c4079df695a40f28f5d636db24e952da051
Jennifer Long <j.alicia.long@gmail.com>
2020-03-01 13:59:38 -0500

    food list display & form

--- fridge_app/views.py
+++ fridge_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views import generic
+from django.urls import reverse_lazy
 
 from .models import Food
 # from fridge_app.forms import FoodForm
@@ -17,9 +18,20 @@ def about(request):
 @method_decorator(login_required, name='dispatch')
 class LogFoodView(generic.CreateView):
     model = Food
-    template_name = 'food_form.html'
-    success_url = 'index/'
+    template_name = 'fridge_app/food_form.html'
+    success_url = reverse_lazy('fridge_app:food')
     fields = ['user', 'food_name', 'exp_date']
+
+@method_decorator(login_required, name='dispatch')
+class FoodView(generic.ListView):
+    template_name = 'fridge_app/food_list.html'
+    context_object_name = 'food_list'
+
+    def get_queryset(self):
+        """
+        Return all goals for the user
+        """
+        return Food.objects.all() # need to filter by user
 
 # May want to use a form class instead for more customization
 # @login_required
--- fridge_project/settings.py
+++ fridge_project/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'crispy_forms',
     'fridge_app.apps.FridgeAppConfig',
     'user_auth.apps.UserAuthConfig',
 ]
+
+# CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
