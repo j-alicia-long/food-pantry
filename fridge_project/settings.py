03f3e14be26814a8a7ed038de70bd57a0673dcad
Jennifer Long <j.alicia.long@gmail.com>
2020-02-29 21:26:14 -0500

    template extend reverse issue

--- fridge_project/settings.py
+++ fridge_project/settings.py
@@ -34,13 +34,13 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'fridge_app.apps.FridgeAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fridge_app.apps.FridgeAppConfig',
     'user_auth.apps.UserAuthConfig',
 ]
 
